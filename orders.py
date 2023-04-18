5108b6734e750346515bf9a638d85ea7a7be7b24
Владислав Яковлев <yakovlev@MacBook-Pro-Vladislav.local>
2023-04-18 23:52:13 +0300

    initial commmit

---
+++ orders.py
@@ -0,0 +1,23 @@
+import psycopg2
+from lib import PgConnect
+
+class Order:
+    customer_id: int
+    courier_id: int
+    message: str
+
+class OrdersTable:
+    def __init__(self, pg):
+        self.pg = pg
+    def create_order(self, customer_id, courier_id=None, message=None, is_finished=False):
+        with self.pg.connection() as conn:
+            with conn.cursor() as cur:
+                sql_query = f"""
+                INSERT INTO orders (customer_id, courier_id, message, is_finished) VALUES ({customer_id}, {courier_id}, '{message}', {is_finished})
+                """
+                print(sql_query)
+                cur.execute(sql_query)
+
+if __name__ == '__main__':
+    pg = PgConnect('localhost', '5432', 'postgres', 'postgres', pw=None, sslmode='disable')
+    OrdersTable(pg).create_order(0, 1, 'pizda')
